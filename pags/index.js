08c189fc339a64da40dbd0a122b1947ae50ea20d
IngraPRJC <palola.lr@hotmail.com>
2021-09-20 13:53:16 -0300

    o

--- pags/index.js
+++ pags/index.js
@@ -1,6 +1,14 @@
-var b1 = document.querySelector('div#menu > p')
-
-b1.style.display = 'inline'
-function m(){
-    b1.style.display = 'inline-block'
+var p = 0
+var t = document.getElementById('b')
+var e = document.querySelector('main')
+function menu(){
+    if(p == 0){
+        t.style.transform = 'translateX(100%)'
+        p++
+        e.style.marginLeft = '50px'
+    }else{
+        t.style.transform = 'translateX(0%)'
+        p = 0
+        e.style.margin = '0px'
+    }
 }
