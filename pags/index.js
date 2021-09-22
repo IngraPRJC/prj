df5b480a9c1440fc52c02a148c500a0ebb5a348e
IngraPRJC <palola.lr@hotmail.com>
2021-09-22 15:15:08 -0300

    s

--- pags/index.js
+++ pags/index.js
@@ -6,9 +6,11 @@ function menu(){
         t.style.transform = 'translateX(100%)'
         p++
         e.style.paddingLeft = "100px"
+        e.style.width = "75%"
     }else{
         t.style.transform = 'translateX(0%)'
         p = 0
         e.style.paddingLeft = "0px"
+        e.style.width = "75%"
     }
 }
