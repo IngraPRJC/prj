e76b2f9525a8ca593aeb4f5c991d32d8bc675ccc
IngraPRJC <palola.lr@hotmail.com>
2021-10-08 17:37:39 -0300

    s

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var cores = ['vermelho', 'amarelo', 'azul', 'marrom', 'verde', 'cinza']
-
+document.getElementById('enviar').disabled = false
 var lm = -1
+var h = []
 // definindo a cor a ser escolhida
 var cor = (Math.random()*5).toFixed(0)
 
@@ -13,97 +14,34 @@ texto.textContent = cores[cor] + ':'
 console.log(cor)
 //lista onde entraram os jogadores com as propriedades vitorias, nome e derrotas
 var jogadores = []
+function add(c){
+    var jogador = jogadores[c]
+    jogador.derrotas += 1
+    exibir(jogadores)
+}
 
-function adicionarvitoria(i){
-  var jogador = jogadores[i]
+function adicionarvitoria(x){
+  var jogador = jogadores[x]
   //acessando a proriedade e alterando
   jogador.vitorias += 1
+  
   //atualizando a tabela
   exibir(jogadores)
 }
-
-function adicionarDerrota(i){
-  var jogador = jogadores[i]
-   //acessando a proriedade e alterando
-  jogador.derrotas += 1
-  jogador.vitorias = jogador.vitorias
- //arrumando um bug, que impedia a vitoria após uma vitoria, adicionanod as condições novamente
-  if (cores[cor] == 'azul'){
-        if(input == "papel" | input == "papelão" | input == 'papel ou papelão' |input == "papel" & m.textContent == 'Errou'| input == "papelão" & m.textContent == 'Errou'| input == 'papel ou papelão' & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
+function garantir(){
+    for(var jh = 0; jh <=2; jh++){
+        mudar()
+        document.getElementById('vermelho').value = ''
+        exibir(jogadores)
+    }
+    for(var hj = 0; hj<=h.length; hj++){
+        while(cores[cor] == h[hj] ){
             mudar()
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-            mudar()
-        }
-    }else if (cores[cor] == 'vermelho'){
-        if(input == "plástico" |input == "plástico" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-            
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'verde'){
-        if(input == "vidro"|input == "vidro" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'amarelo'){
-        if(input == "ferro" | input == "metal"|input == "ferro" & m.textContent == 'Errou'| input == "metal" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-            
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'marrom'){
-        if(input == "orgânico" | input == "lixo orgânico"|input == "orgânico" & m.textContent == 'Errou'|input == "lixo orgânico" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'cinza'){
-        if(input == "não recilável"|input == "não recilável" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
+            verificar()
         }
     }
-  //atualizando a tabela
-  exibir(jogadores)
 }
- 
+
 //cria a tebela
 function exibir(jogadores) {
     var element = ""
@@ -112,19 +50,20 @@ function exibir(jogadores) {
           element += "<td>"+ jogadores[i].vitorias +"</td>"
          
           element += "<td>"+ jogadores[i].derrotas + "</td>"
-         
+          element += "<td>"+ (jogadores[i].vitorias/(jogadores[i].derrotas + jogadores[i].vitorias)*100).toString() + "%"+"</td>"
           element += "</tr>"
           
     }
     
     
     var tela = document.getElementById("tabelaJogadores")
+    for(var yu = 0; yu <= 2; yu++)
     tela.innerHTML = element
     
   }
   exibir(jogadores)
   //envia o nome e propriedades do jogador novo para o array no inicio do código
-  function Envia() {
+function Envia() {
     var p = document.getElementById("aviso")
     var nome = document.getElementById("nome").value
     if ( nome != ""){
@@ -141,9 +80,27 @@ function exibir(jogadores) {
   
       p.innerHTML = "Escreva um nome"
     }
-  }
-  
-  
+}
+function errado() {
+    m.textContent = "Errou"
+    add(lm)
+    verificar()
+   
+    exibir(jogadores)
+    garantir()
+
+
+}
+function certo(){
+    m.textContent = 'acertou'
+    adicionarvitoria(lm)
+    exibir(jogadores)
+    h.push(cores[cor])
+    h = h
+    verificar()
+    garantir()
+    
+}
 //muda a cor da lixeira
 function mudar(){
     cor = (Math.random()*5).toFixed(0)
@@ -151,84 +108,99 @@ function mudar(){
     m.textContent = ""
     var input = document.getElementById('vermelho')
     input.value = ""
+    h = h
+    for(var hj = 0; hj<=h.length; hj++){
+        while(cores[cor] == h[hj]){
+            cor = (Math.random()*5).toFixed(0)
+            texto.textContent = cores[cor] + ":"
+            verificar()
+        }
+    }
+    
+}
+function verificar(){
+    if (cores[cor] == 'azul'){
+        if(input == "papel" | input == "papelão" | input == 'papel ou papelão' |input == "papel" & m.textContent == 'Errou'| input == "papelão" & m.textContent == 'Errou'| input == 'papel ou papelão' & m.textContent == 'Errou'){
+           certo()
+           
+        }
+    }else if (cores[cor] == 'vermelho'){
+        if(input == "plástico" |input == "plástico" & m.textContent == 'Errou'){
+            certo()
+        }
+    }else if (cores[cor] == 'verde'){
+        if(input == "vidro"|input == "vidro" & m.textContent == 'Errou'){
+            certo()
+        }
+    }else if (cores[cor] == 'amarelo'){
+        if(input == "ferro" | input == "metal"|input == "ferro" & m.textContent == 'Errou'| input == "metal" & m.textContent == 'Errou'){
+            certo()
+            
+        }
+    }else if (cores[cor] == 'marrom'){
+        if(input == "orgânico" | input == "lixo orgânico"|input == "orgânico" & m.textContent == 'Errou'|input == "lixo orgânico" & m.textContent == 'Errou'){
+            certo()
+        }
+    }else if (cores[cor] == 'cinza'){
+        if(input == "não reciclável"|input == "não reciclável" & m.textContent == 'Errou'){
+            certo()
+        }
+    }
 }
 //envia a resposta, se estiver correta adiciona 1 ponto a tabela de vitorias e o mesmo para derrotas
 function enviar(){
     var input = document.getElementById('vermelho').value.toLowerCase()
-    
-    console.log(input)
-    if (cores[cor] == 'azul'){
-        if(input == "papel" | input == "papelão" | input == 'papel ou papelão' |input == "papel" & m.textContent == 'Errou'| input == "papelão" & m.textContent == 'Errou'| input == 'papel ou papelão' & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-            mudar()
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-            mudar()
-        }
-    }else if (cores[cor] == 'vermelho'){
-        if(input == "plástico" |input == "plástico" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-            
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'verde'){
-        if(input == "vidro"|input == "vidro" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'amarelo'){
-        if(input == "ferro" | input == "metal"|input == "ferro" & m.textContent == 'Errou'| input == "metal" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-            
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'marrom'){
-        if(input == "orgânico" | input == "lixo orgânico"|input == "orgânico" & m.textContent == 'Errou'|input == "lixo orgânico" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
-    }else if (cores[cor] == 'cinza'){
-        if(input == "não recilável"|input == "não recilável" & m.textContent == 'Errou'){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-            exibir(jogadores)
-        }else{
-            m.textContent = "Errou"
-            
-            adicionarDerrota(lm)
-            exibir(jogadores)
-            input.innerHTML = ''
-        }
+    var jogador = jogadores[lm]
+    if(jogador.vitorias == 5){
+        finalizar()
     }
+    console.log(input)
+    if(jogador.vitorias != 5){
+        if (cores[cor] == 'azul'){
+            if(input == "papel" | input == "papelão" | input == 'papel ou papelão' |input == "papel" & m.textContent == 'Errou'| input == "papelão" & m.textContent == 'Errou'| input == 'papel ou papelão' & m.textContent == 'Errou'){
+            certo()
+            
+            
+            }else{
+                errado()
+            }
+        }else if (cores[cor] == 'vermelho'){
+            if(input == "plástico" |input == "plástico" & m.textContent == 'Errou'){
+                certo()
+                
+            }else{
+                errado()
+            }
+        }else if (cores[cor] == 'verde'){
+            if(input == "vidro"|input == "vidro" & m.textContent == 'Errou'){
+                certo()
+                
+            }else{
+                errado()
+            }
+        }else if (cores[cor] == 'amarelo'){
+            if(input == "ferro" | input == "metal"|input == "ferro" & m.textContent == 'Errou'| input == "metal" & m.textContent == 'Errou'){
+                certo()
+                
+                
+            }else{
+            errado()
+            }
+        }else if (cores[cor] == 'marrom'){
+            if(input == "orgânico" | input == "lixo orgânico"|input == "orgânico" & m.textContent == 'Errou'|input == "lixo orgânico" & m.textContent == 'Errou'){
+                certo()
+                
+            }else{
+            errado()
+            }
+        }else if (cores[cor] == 'cinza'){
+            if(input == "não reciclável"|input == "não reciclável" & m.textContent == 'Errou'){
+                certo()
+            
+            }else{
+            errado()
+            }
+    }   }
     input = document.getElementById('vermelho')
     input.value = " "
 }
@@ -237,37 +209,10 @@ function finalizar(){
       document.getElementById('mud').disabled = true
       document.getElementById('env').disabled = true
       document.getElementById('enviar').disabled = false
+      verificar()
+      var ati = 0
+      
 }
 /*Azul: Papel e Papelão</span>Vermelho: Plástico</span>Verde: vidroAmarelo: metal</span><span style="margin-left: 10px;">Marrom: organico</span>Cinza: não reciclavel*/
 // criando um objeto que armazena propriedades diferentes
-/*if (cores[cor] == 'azul'){
-    if(input == "papel" | input == "papelão" | input == 'papel ou papelão'){
-        m.textContent = 'acertou'
-        adicionarvitoria(lm)
-    }
-    }else if (cores[cor] == 'vermelho'){
-        if(input == "plástico"){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-        }
-    }else if (cores[cor] == 'verde'){
-        if(input == "vidro"){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-        }
-    }else if (cores[cor] == 'amarelo'){
-        if(input == "ferro" | input == "metal"){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-        }
-    }else if (cores[cor] == 'marrom'){
-        if(input == "orgânico" | input == "lixo orgânico"){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-        }
-    }else if (cores[cor] == 'cinza'){
-        if(input == "não recilável"){
-            m.textContent = 'acertou'
-            adicionarvitoria(lm)
-        } */
-document.getElementById('enviar').disabled = false
+
